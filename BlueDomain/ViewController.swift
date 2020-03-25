b0022c7a8e4d4ebc488f5749d40a96d2bbdb579c
Faizy51 <faizulla49@yahoo.co.uk>
2020-03-25 19:21:51 +0530

    Initial Commit

---
+++ BlueDomain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlueDomain
+//
+//  Created by Faizyy on 25/03/20.
+//  Copyright © 2020 faiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
